471e24f5b21f0fa781b1975e8d9fe42f4dc70bca
Katie Harold <kharold@sjccs142.saintjoe.edu>
2015-02-27 11:47:29 -0500

    // made a table of random info

---
+++ randomInfo.js
@@ -0,0 +1,9 @@
+var RANDOM = [
+  {color: "blue", number: 18, shape: "circle", state: "West Virginia"},
+  {color: "red", number: 76, shape: "square", state: "Iowa"},
+  {color: "green", number: 92, shape: "triangle", state: "New York"},
+  {color: "yellow", number: 19, shape: "rectangle", state: "Florida"}
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = RANDOM;
